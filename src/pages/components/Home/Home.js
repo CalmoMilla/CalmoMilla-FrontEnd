d2f6de8738c11c07f1a6cb789152f5499b76ea5a
Arthur Selingin <88356338+artusk8dev@users.noreply.github.com>
2024-05-15 20:43:49 -0300

    🐛  @selingindev  ~fix corrigindo bug cujo as imagens não estavam sendo carregadas .

--- src/pages/components/Home/Home.js
+++ src/pages/components/Home/Home.js
@@ -45,15 +45,15 @@ export default function Home() {
       <Header/>
       <PaginaInicial/>
 
-      <Funcionalidade imagem={"/assets/funcionalidades/img-brain.png"} 
+      <Funcionalidade imagem={"/assets/home/funcionalidades/img-brain.png"} 
       cor={"text-amarelo1"} titulo={"Jogos Mentais"} posicao={true} 
       texto={"Explore uma nova dimensão de cuidado cerebral com nosso aplicativo de exercícios mentais dedicado à prevenção da demência. Com uma série de desafios cognitivos cientificamente projetados, oferecemos uma maneira envolvente e estimulante de manter sua mente afiada e saudável. Desde jogos de memória até quebra-cabeças desafiadores, nossa plataforma intuitiva oferece uma abordagem holística para fortalecer suas habilidades cognitivas, permitindo que você aproveite ao máximo cada momento de lucidez."}/>
 
 
-      <Funcionalidade imagem={"/assets/funcionalidades/img-yoga.png"} 
+      <Funcionalidade imagem={"/assets/home/funcionalidades/img-yoga.png"} 
       cor={"text-verdeagua"} titulo={"Relaxamento"} posicao={false} texto={"“Relaxamento” incluem sessões guiadas de mindfulness, meditação e técnicas de respiração para ajudar os usuários a reduzir o estresse e a ansiedade. Encontre tranquilidade e bem-estar onde quer que você esteja com nosso aplicativo de relaxamento e meditação guiada. Com uma variedade de práticas meditativas adaptadas às suas necessidades, desde redução de estresse até melhoria do sono, oferecemos uma jornada personalizada para o equilíbrio mental. Nossa plataforma intuitiva permite que você explore uma biblioteca de áudios e vídeos guiados por especialistas, proporcionando momentos de serenidade e autoconexão em meio ao caos do dia a dia."}/>
 
-      <Funcionalidade imagem={"/assets/funcionalidades/img-psicocall.png"} 
+      <Funcionalidade imagem={"/assets/home/funcionalidades/img-psicocall.png"} 
       cor={"text-verde2"} titulo={"Integração com Psicólogos"} posicao={true} texto={"Descubra uma nova abordagem para o autocuidado emocional com nosso aplicativo de consultas psicológicas online. Com uma interface intuitiva e segura, conectamos você a uma rede de psicólogos qualificados, prontos para ajudá-lo a enfrentar desafios emocionais onde quer que você esteja. Desde sessões de terapia por vídeo até mensagens instantâneas, oferecemos suporte flexível e conveniente para atender às suas necessidades, garantindo que você possa priorizar sua saúde mental de forma acessível e sem complicações. Baixe agora e descubra uma nova maneira de encontrar equilíbrio emocional na palma da sua mão."}/>
 
       <ComoFunciona/>
