b551969e4dfb04bc662b5a1e098f70828ca0031e
vitorvhsilva <dndragonbr@gmail.com>
2024-05-16 18:41:30 -0300

    @vitorvhsilva Responsividade das estatisticas.

--- src/pages/components/Usuario/VisaoGeral/Estatistica.js
+++ src/pages/components/Usuario/VisaoGeral/Estatistica.js
@@ -1,8 +1,8 @@
 export default function Estatistica(props) {
   return (
-    <div className="flex justify-around items-center py-5">
-      <h3 className="w-[30%] text-3xl font-nunito text-roxo">{props.titulo}</h3>
-      <div className="w-[55%] h-6 bg-roxo/60">
+    <div className="flex justify-around md:flex-row flex-col items-center py-5">
+      <h3 className="w-[80%] md:w-[30%] text-2xl md:text-3xl font-nunito text-roxo">{props.titulo}</h3>
+      <div className="w-[80%] md:w-[55%] h-6 bg-roxo/60">
         <div className={`w-[${props.resul}%] h-full bg-roxo`}></div>
       </div>
     </div>
--- src/pages/components/Usuario/VisaoGeral/Estatisticas.js
+++ src/pages/components/Usuario/VisaoGeral/Estatisticas.js
@@ -3,9 +3,9 @@ import Estatistica from "./Estatistica"
 export default function Estatisticas() {
   return (
     <>
-      <div className="p-8">
-        <h1 className="text-5xl font-nunito text-roxo">Visão Geral</h1>
-        <h2 className="text-3xl font-nunito text-roxo pt-10">Suas estatísticas</h2>
+      <div className="pt-2 m-auto md:p-8 w-[80%] md:w-auto">
+        <h1 className="text-3xl md:text-5xl font-nunito text-roxo">Visão Geral</h1>
+        <h2 className="text-2xl md:text-3xl font-nunito text-roxo pt-10">Suas estatísticas</h2>
       </div>
       <Estatistica titulo={"Memória"} resul={30}/>
       <Estatistica titulo={"Resolução de Problemas"} resul={70}/>
