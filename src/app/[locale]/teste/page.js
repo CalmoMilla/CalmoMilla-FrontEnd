193c025256fd97c9b8772168f8a54af8c3568482
vitorvhsilva <dndragonbr@gmail.com>
2024-05-19 03:47:57 -0300

    @vitorvhsilva Começo do calendário sendo feito

--- src/app/[locale]/teste/page.js
+++ src/app/[locale]/teste/page.js
@@ -1,8 +1,8 @@
 import Estatisticas from "@/pages/components/Usuario/VisaoGeral/Estatisticas";
-
+import CalendarioHumor from "@/pages/components/Usuario/CalendarioHumor/CalendarioHumor";
 
 export default function Page() {
   return (
-    <Estatisticas/>
+    <CalendarioHumor/>
   );
 }
---
+++ src/pages/components/Usuario/CalendarioHumor/LinhaCalendario.js
@@ -0,0 +1,13 @@
+export default function LinhaCalendario() {
+  return (
+    <div className="grid grid-cols-4 md:grid-cols-7 gap-4 py-5">
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+      <div className="w-[25%] rounded-full h-10 bg-roxo/50"></div>
+    </div>
+  )
+}
