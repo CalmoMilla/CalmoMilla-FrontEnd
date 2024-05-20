2e879b571026b6e12aa32ffda0bc84a9a11ba8c7
augusta <vi_augusta@usp.br>
2024-05-20 19:03:57 -0300

    @AvgvstaDev materia principal

--- src/pages/components/Usuario/MateriaBlog/MateriaPrincipal/MateriaPrincipal.js
+++ src/pages/components/Usuario/MateriaBlog/MateriaPrincipal/MateriaPrincipal.js
@@ -2,10 +2,12 @@ import Image from "next/image"
 
 export default function MateriaPrincipal() {
     return(
-        <div>
-            <h2>Lorem ipsum dolor sit amet. Ut dignissimos neque</h2>
-            <Image src={"/assets/usuario/materiablog/materia4.png"} alt="Imagem da matéria principal" width={450} height={300}/>
-            <p>Lorem ipsum dolor sit amet. In eligendi molestiae est quia suscipit hic aspernatur nesciunt aut molestiae laborum. Sit labore quibusdam ut repellendus quod sed rerum accusamus est omnis quos et harum corporis est omnis dolores nam omnis minima. Qui laborum dolore et voluptates maxime qui voluptas repellendus et quos deleniti et inventore quas ut neque galisum. Et quia officiis qui sint inventore qui distinctio praesentium est quia ipsam quo nulla nulla.
+        <div className="w-11/12 space-y-7 m-auto">
+            <h2 className="font-calistoga">Lorem ipsum dolor sit amet. Ut dignissimos neque</h2>
+            
+            <Image width={1360} height={450} src={"/assets/usuario/materiablog/materia4.png"} alt="Imagem da matéria principal"/>
+           
+            <p className="font-nunito text-left">Lorem ipsum dolor sit amet. In eligendi molestiae est quia suscipit hic aspernatur nesciunt aut molestiae laborum. Sit labore quibusdam ut repellendus quod sed rerum accusamus est omnis quos et harum corporis est omnis dolores nam omnis minima. Qui laborum dolore et voluptates maxime qui voluptas repellendus et quos deleniti et inventore quas ut neque galisum. Et quia officiis qui sint inventore qui distinctio praesentium est quia ipsam quo nulla nulla.
 Sit quam quam eos consequatur quae vel deserunt aliquam cum consequuntur iste eum repellat perspiciatis et adipisci deserunt eum iure repellat. Sit vero commodi ut sequi autem id dolorem porro quo nostrum omnis aut enim illum sit voluptates ducimus et Quis praesentium. Non quia architecto ut iste alias aut nemo atque At provident ducimus eum omnis commodi id debitis velit. Et alias nihil est reiciendis quasi est tempore quis ut sunt deserunt non fugiat veritatis sed totam velit in sint ipsa.</p>
         </div>
     )
