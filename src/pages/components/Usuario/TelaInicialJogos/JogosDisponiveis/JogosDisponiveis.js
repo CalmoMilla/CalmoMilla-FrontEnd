6463be95e10e38abe7bd73abc7e46a03154c1a7b
vitorvhsilva <dndragonbr@gmail.com>
2024-05-20 01:55:31 -0300

    @vitorvhsilva Utilização dos props.

--- src/pages/components/Usuario/TelaInicialJogos/JogosDisponiveis/JogosDisponiveis.js
+++ src/pages/components/Usuario/TelaInicialJogos/JogosDisponiveis/JogosDisponiveis.js
@@ -4,12 +4,12 @@ export default function JogosDisponiveis() {
   return (
     <div className="w-[70%] h-screen">
       <div className="w-[100%] h-[100%] lg:h-[50%] flex justify-around items-center flex-col lg:flex-row">
-        <JogoDisponivel titulo={"Jogo da Memoria"} texto={"Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum "}/>
-        <JogoDisponivel titulo={"Sudoku"} texto={"Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum "}/>
+        <JogoDisponivel titulo={"Jogo da Memoria"} texto={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum "}/>
+        <JogoDisponivel titulo={"Sudoku"} texto={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum "}/>
       </div>
       <div className="w-[100%] h-[100%] lg:h-[50%] flex justify-around items-center flex-col lg:flex-row">
-        <JogoDisponivel titulo={"Xadrez"} texto={"Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum "}/>
-        <JogoDisponivel titulo={"Palavras Cruzadas"} texto={"Lorem ipsum lorem ipsum Lorem ipsum lorem ipsum "}/>
+        <JogoDisponivel titulo={"Xadrez"} texto={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum "}/>
+        <JogoDisponivel titulo={"Palavras Cruzadas"} texto={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum "}/>
       </div>
     </div>
   )
