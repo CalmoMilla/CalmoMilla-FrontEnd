fed92c480bde0a4c52b2c9dc0a9cf71a79b726b0
vitorvhsilva <dndragonbr@gmail.com>
2024-05-20 02:50:09 -0300

    @vitorvhsilva Responsividade perfil do usuário

--- src/pages/components/Usuario/Perfil/InfoConta/InfoConta.js
+++ src/pages/components/Usuario/Perfil/InfoConta/InfoConta.js
@@ -1,20 +1,20 @@
 export default function InfoConta() {
   return (
     <>
-      <div className="w-[100%] h-[30%] flex justify-around items-center">
+      <div className="w-[100%] h-[30%] flex md:flex-row flex-col justify-around items-center">
         <div className="w-60 h-60 bg-gray-500 rounded-full"></div>
-        <div className="w-fit h-[100%]">
+        <div className="md:pt-0 pt-10 text-center md:text-left w-fit h-[100%]">
           <p className="text-3xl font-nunito pb-10">Olá fulano!</p>
           <p className="text-2xl font-nunito">Aqui estão suas informações pessoais na plataforma</p>
         </div>
       </div>
-      <div className="pl-28 pt-20">
+      <div className="pl-8 lg:pl-28 pt-20">
         <h1 className="text-3xl py-5">Informações da conta</h1>
-        <div className="w-[30%] flex justify-between items-center py-5">
+        <div className="lg:w-[30%] w-[80%] md:flex-row flex-col md:items-center flex justify-between py-5">
           <p className="text-xl">Email</p>
           <p className="text-xl">seuemail@gmail.com</p>
         </div>
-        <div className="w-[30%] flex justify-between items-center py-5">
+        <div className="lg:w-[30%] w-[80%] md:flex-row flex-col flex justify-between md:items-center py-5">
           <p className="text-xl">Senha</p>
           <p className="text-xl">**********</p>
         </div>
--- src/pages/components/Usuario/Perfil/InfoPessoal/CampoPessoal.js
+++ src/pages/components/Usuario/Perfil/InfoPessoal/CampoPessoal.js
@@ -1,6 +1,6 @@
 export default function CampoPessoal(props) {
   return (
-    <div className="w-[80%] flex justify-between items-center py-5">
+    <div className="w-[80%] flex justify-between md:flex-row flex-col md:items-center py-5">
       <p className="text-xl">{props.texto1}</p>
       <p className="text-xl">{props.texto2}</p>
     </div>
