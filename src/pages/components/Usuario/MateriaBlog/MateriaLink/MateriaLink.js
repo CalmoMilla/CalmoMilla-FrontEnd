508a8c62d433a7b771c8310d35549e76012ba431
augusta <vi_augusta@usp.br>
2024-05-20 19:23:13 -0300

    @AvgvstaDev finalização da matéria principal

--- src/pages/components/Usuario/MateriaBlog/MateriaLink/MateriaLink.js
+++ src/pages/components/Usuario/MateriaBlog/MateriaLink/MateriaLink.js
@@ -4,7 +4,7 @@ export default function MateriaLink(props) {
     <div className={`w-[25%] h-[80vh] hover:scale-110 transition duration-500 ease-in-out bg-cover bg-no-repeat rounded-xl`} style={{backgroundImage: `url(${props.src})`}}>
       
       <div className="h-full w-full bg-preto/30 rounded-xl">
-         <p className={`text-branco text-2xl font-bold pl-10 ${props.baixo ? "pt-[130%]" : "pt-[15%]"}`}>{props.text}</p>
+         <p className={`text-branco text-2xl font-bold font-nunito pl-10 ${props.baixo ? "pt-[120%]" : "pt-[15%]"}`}>{props.text}</p>
       </div>
     </div>
   )
