357232720d1164ce70b34c942ab5562e90096308
vitorvhsilva <dndragonbr@gmail.com>
2024-05-21 15:10:23 -0300

    @vitorvhsilva Tentando ajustar bug no deploy

--- src/pages/components/Usuario/PaginaInicial/ExercicioDia/ExercicioDia.js
+++ src/pages/components/Usuario/PaginaInicial/ExercicioDia/ExercicioDia.js
@@ -12,7 +12,7 @@ export default function ExercicioDia(props) {
         <div className="flex flex-col justify-center">
           <h2 className="text-branco text-5xl ">{props.titulo}</h2>
           <p className="text-branco text-2xl py-2">{props.desc}</p>
-          <Link href={props.link} className="w-fit">
+          <Link href={props.link ? props.link : ""} className="w-fit">
             <button className="text-xl bg-branco rounded-full hover:bg-amarelo2 text-amarelo1 hover:text-branco transition duration-300 ease-out py-4 px-10 font-bold">Vamos começar?</button>
           </Link>
         </div>
