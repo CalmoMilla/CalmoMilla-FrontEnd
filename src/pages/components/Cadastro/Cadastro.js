e8786ccb8ac728d67e118b09bea0b49f9bec3748
Arthur Selingin <88356338+artusk8dev@users.noreply.github.com>
2024-05-22 09:14:04 -0300

    🎨 @selingindev ~mudanças pertinentes para o responsivo

--- src/pages/components/Cadastro/Cadastro.js
+++ src/pages/components/Cadastro/Cadastro.js
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function Cadastro() {
   return (
     <div className="flex w-screen h-screen overflow-hidden items-center justify-center bg-no-repeat bg-center lg:bg-[url('/assets/Cadastro/LG.png')] xl:bg-[url('/assets/Cadastro/1440x1024.png')] 2xl:bg-[url('/assets/Cadastro/Login_-_1920x1080.png')] ">
-      <div className="m-auto flex justify-center items-center h-[90%] w-[80%] border rounded-lg shadow-lg">
-        <div className=" flex justify-center items-center w-full h-auto lg:w-[60%]">
+      <div className="m-auto flex justify-center lg:items-center h-[90%] w-[80%] border rounded-lg shadow-lg xs:items-start " >
+        <div className=" flex justify-center lg:items-center w-full h-auto lg:w-[60%] xs:mt-6">
           <CadastroPsico />
         </div>
         <div className="relative w-[40%] h-full hidden lg:block">
