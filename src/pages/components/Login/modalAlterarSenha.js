7c82ebeee952dd0fc6c76ee0fd082e7881e369b4
vitorvhsilva <dndragonbr@gmail.com>
2024-05-26 16:05:02 -0300

    @vitorvhsilva Testando arrumar deploy no Vercel 3 (modal)

--- src/pages/components/Login/modalAlterarSenha.js
+++ src/pages/components/Login/modalAlterarSenha.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { IoCloseSharp } from "react-icons/io5";
+import { createRoot } from 'react-dom/client';
 
 const Modal = ({ onClose, children, title }) => {
   const handleCloseClick = (e) => {
@@ -10,44 +11,52 @@ const Modal = ({ onClose, children, title }) => {
     }
   };
 
-  const modalContent = (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="xs:w-[80%] xs:h-[80%] md:w-[500px]  md:h-[600px] bg-white rounded-lg p-4">
-        <div className="flex justify-end text-2xl items-center">
-          <div
-            className="h-20 w-screen
-           text-center flex justify-center items-center "
-          >
-            <h2 className="font-calistoga w-full sm:text-3xl">
-              Recupere sua senha{" "}
-            </h2>
-          </div>
-          <button
-            className="absolute w-auto h-auto p-0  sm:hidden"
-            onClick={handleCloseClick}
-          >
-            <IoCloseSharp size={20} />
-          </button>
+  useEffect(() => {
 
-          <button
-            className="sm:flex sm:absolute w-auto h-auto p-0 py-2 hidden"
-            onClick={handleCloseClick}
-          >
-            <IoCloseSharp size={45} />
-          </button>
-        </div>
-        {title && <h1 className="text-xl font-bold">{title}</h1>}
-        <div className="w-full h-full">
-          {typeof window !== 'undefined' && children}
+    const modalContent = (
+      <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+        <div className="xs:w-[80%] xs:h-[80%] md:w-[500px]  md:h-[600px] bg-white rounded-lg p-4">
+          <div className="flex justify-end text-2xl items-center">
+            <div
+              className="h-20 w-screen
+             text-center flex justify-center items-center "
+            >
+              <h2 className="font-calistoga w-full sm:text-3xl">
+                Recupere sua senha{" "}
+              </h2>
+            </div>
+            <button
+              className="absolute w-auto h-auto p-0  sm:hidden"
+              onClick={handleCloseClick}
+            >
+              <IoCloseSharp size={20} />
+            </button>
+  
+            <button
+              className="sm:flex sm:absolute w-auto h-auto p-0 py-2 hidden"
+              onClick={handleCloseClick}
+            >
+              <IoCloseSharp size={45} />
+            </button>
+          </div>
+          {title && <h1 className="text-xl font-bold">{title}</h1>}
+          <div className="w-full h-full">
+            {children}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  
+    const portalElement = document.getElementById("modal-root");
+    const root = createRoot(portalElement);
+  
+    root.render(modalContent);
+  
+    return () => {
+      root.unmount();
+    };
+  },)
 
-  return ReactDOM.createPortal(
-    modalContent,
-    document.getElementById("modal-root")
-  );
 };
 
 export default Modal;
