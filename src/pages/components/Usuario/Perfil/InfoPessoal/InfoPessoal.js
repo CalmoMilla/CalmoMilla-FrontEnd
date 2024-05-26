b6c3ea9fb0ca6a45c053b7125caf8961802c6cb3
vitorvhsilva <dndragonbr@gmail.com>
2024-05-26 15:12:08 -0300

    @vitorvhsilva Perfil dinâmico com o login.

--- src/pages/components/Usuario/Perfil/InfoPessoal/InfoPessoal.js
+++ src/pages/components/Usuario/Perfil/InfoPessoal/InfoPessoal.js
@@ -1,11 +1,23 @@
+"use client"
+
 import CampoPessoal from "./CampoPessoal";
+import { useEffect, useState } from "react";
 
 export default function InfoPessoal() {
+
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() =>  {
+    const usuarioStorage = localStorage.getItem("usuario")
+    const infoUsuario = JSON.parse(usuarioStorage)
+    setUsuario(infoUsuario)
+  }, [])
+
   return (
     <>
       <h2 className="text-5xl text-amarelo1 font-calistoga text-center py-5">Informações Pessoais</h2>
       <div className="flex justify-center flex-col items-center">     
-        <CampoPessoal texto1={"Nome"} texto2={"Insira um nome"}/>
+        <CampoPessoal texto1={"Nome"} texto2={usuario ? usuario.nome : ""}/>
         <CampoPessoal texto1={"Data de nascimento"} texto2={"xx/xx/xxxx"}/>
         <CampoPessoal texto1={"Gênero"} texto2={"Indefinido"}/>
         <CampoPessoal texto1={"Idioma"} texto2={"Português"}/>
