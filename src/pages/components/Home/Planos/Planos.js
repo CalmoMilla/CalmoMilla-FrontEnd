8f9d3487354cd88d1d860db766336a838970b50e
vitorvhsilva <dndragonbr@gmail.com>
2024-06-02 23:03:10 -0300

    @vitorvhsilva Arrumando os Planos na Plataforma.

--- src/pages/components/Home/Planos/Planos.js
+++ src/pages/components/Home/Planos/Planos.js
@@ -7,7 +7,7 @@ export default function Planos() {
       <h2 className="text-verdeagua text-7xl font-calistoga text-center pt-10 pb-24 lg:pb-40 ">Planos do Usuário</h2>
       <div className="sm:flex-col lg:flex lg:flex-row justify-around pb-12">
         <Plano titulo={"Remova os ADS"} preco={"R$15.00"} plano={"Mensal"}/>
-        <Plano titulo={"Remova os ADS"} preco={"R$30.00"} plano={"Vitalício"}/>
+        <Plano titulo={"Remova os ADS"} preco={"R$90.00"} plano={"Anual"}/>
       </div>
     </div>
   )
