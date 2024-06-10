987f39f29bd1e6467bd5501f82c4492c9a7d189f
Arthur Selingin <88356338+artusk8dev@users.noreply.github.com>
2024-06-10 10:50:08 -0300

    @selingindev @vitorvhsilva  ~fazendo requisição do email

--- src/pages/components/Login/EsqueciSenha.js
+++ src/pages/components/Login/EsqueciSenha.js
@@ -1,6 +1,15 @@
 export default function EsqueciSenha() {
-  return (
-    <div className=" w-[90%] h-[90%] flex flex-col items-center justify-center mx-auto ">
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    let recuperarSenha = {
+      email: data.get("emailRedefinir"),
+    };
+    console.log(recuperarSenha)
+  }
+  return(
+  <div className=" w-[90%] h-[90%] flex flex-col items-center justify-center mx-auto ">
       <div className="flex-col flex md:w-[70%] xs:w-[100%] h-full">
         <form className="grid-cols-2 h-[80%]">
           <div className="w-full h-[75%] mt-20">
@@ -11,8 +20,8 @@ export default function EsqueciSenha() {
               <input
                 className="border-none border-b-2  font-nunito   placeholder:text-black   text-md w-full p-2"
                 type="text"
-                id="email"
-                name="email"
+                id="emailRedefinir"
+                name="emailRedefinir"
                 placeholder="Coloque seu Email."
                 required
               />
@@ -22,6 +31,7 @@ export default function EsqueciSenha() {
             <button
               className="w-80 h-12 bg-amarelo2 justify-center p-0 rounded-full text-white "
               type="submit"
+              onClick={onSubmit}
             >
               Recuperar
             </button>
