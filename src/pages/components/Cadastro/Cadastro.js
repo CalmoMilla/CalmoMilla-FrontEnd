d6218a37188c67592a5747f870000b964f423ed1
vitorvhsilva <dndragonbr@gmail.com>
2024-06-10 23:21:25 -0300

    @vitorvhsilva Teste no Vercel 5

--- src/pages/components/Cadastro/Cadastro.js
+++ src/pages/components/Cadastro/Cadastro.js
@@ -46,9 +46,17 @@ export default function Cadastro() {
 
               <div className="flex flex-col justify-center lg:items-center w-full h-auto 2xl:items-start lg:w-[100%] mb-auto ">
                 {isUser ? (
-                  status !== 'loading' ? <CadastroUser session={session} /> : <div>Carregando...</div>
+                  status !== 'loading' ? (
+                    <CadastroUser session={session} />
+                  ) : (
+                    <div>Carregando dados do usuário...</div>
+                  )
                 ) : (
-                  status !== 'loading' ? <CadastroPsico session={session} /> : <div>Carregando...</div>
+                  status !== 'loading' ? (
+                    <CadastroPsico session={session} />
+                  ) : (
+                    <div>Carregando dados do psicólogo...</div>
+                  )
                 )}
 
                 <div className="xs:mx-auto md:w-[70%] h-5 m-auto xs:mt-4 2xl:mt-0 xs:text-center p-0 mb-4 lg:text-start">
