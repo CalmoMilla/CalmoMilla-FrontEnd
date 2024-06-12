18e9d48d58408f07dfdc371b0a5f0dc54a7cca00
vitorvhsilva <dndragonbr@gmail.com>
2024-06-12 02:30:27 -0300

    @vitorvhsilva Ajustes na estilização feito

--- src/pages/components/Psicologo/PacientesVinculados/InfoPaciente.js
+++ src/pages/components/Psicologo/PacientesVinculados/InfoPaciente.js
@@ -3,14 +3,14 @@ import { IoIosClose } from "react-icons/io";
 
 export default function InfoPaciente({nome, idade, cidade, onClose}) {
   return (
-    <div className="w-[15%] h-fit shadow-2xl py-5 rounded-xl flex flex-col justify-center items-center gap-4">
+    <div className="w-[15%] h-fit shadow-2xl py-5 rounded-xl flex flex-col justify-center items-center gap-8">
       <div className="w-full flex justify-end">
         <IoIosClose className="w-16 h-16 hover:cursor-pointer text-preto hover:text-amarelo1 duration-500 ease-in-out"
         onClick={onClose}/>
       </div>
       <Image src={"https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"} alt={"Imagem do Usuario"} width={200} height={200} className="rounded-full"/>
-      <h2 className="font-nunito text-xl font-bold">{nome}</h2>
-      <h2 className="font-nunito text-xl font-bold">{idade}</h2>
+      <h2 className="font-nunito text-3xl font-bold">{nome}</h2>
+      <h2 className="font-nunito text-xl font-bold">{idade} anos</h2>
       <h2 className="font-nunito text-xl font-bold">{cidade}</h2>
 
       <button className="font-nunito px-8 py-3 bg-amarelo1 hover:bg-amarelo2 duration-500 ease-in-out rounded-3xl">Abrir perfil</button>
--- src/pages/components/Psicologo/PacientesVinculados/PacienteVinculado.js
+++ src/pages/components/Psicologo/PacientesVinculados/PacienteVinculado.js
@@ -7,7 +7,7 @@ export default function PacienteVinculado({id, nome, idade, cidade, handleClick}
         <Image src={"https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"} alt={"Imagem do Usuario"} width={100} height={100} className="rounded-full"/>
       </div>
       <div className="w-full h-[45%] py-2 px-5">
-        <h2 className="font-nunito text-3xl">{nome}</h2>
+        <h2 className={`font-nunito ${nome.length > 10 ? "text-2xl" : "text-3xl"}`}>{nome}</h2>
         <p className="font-nunito text-xl">{idade}</p>
         <p className="font-nunito text-xl">{cidade}</p>
       </div>
