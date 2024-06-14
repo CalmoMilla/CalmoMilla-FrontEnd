ddc6432ea00f3b56c4d785ac6b4c2e7c5c113e3f
vitorvhsilva <dndragonbr@gmail.com>
2024-06-14 10:47:21 -0300

    @vitorvhsilva @APBielzinx Modal das Emoções quase completo

--- src/pages/components/Usuario/PaginaInicial/Calendario/LinhaEmocoes.js
+++ src/pages/components/Usuario/PaginaInicial/Calendario/LinhaEmocoes.js
@@ -2,14 +2,29 @@
 
 import { useEffect, useState } from "react"
 import { BuscarInfoEmocoes } from "@/pages/api/usuario/UsuarioService";
+import ModalInfoEmocao from "./ModalInfoEmocao";
 
 export default function LinhaEmocoes() {
 
   const [emocoes, setEmocoes] = useState(null)
+  const [modal, setModal] = useState(false)
+  const [infoModal, setInfoModal] = useState([])
+  const [emoji, setEmoji] = useState("")
 
   useEffect(() => {
     buscarEmocoes()
   }, []);
+
+  const onClickEmocao = (emocao, emoji) => {
+    console.log(emocao)
+    const valoresEmocoes = Object.values(emocao);
+    console.log(valoresEmocoes)
+    setInfoModal(valoresEmocoes)
+    setEmoji(emoji)
+
+
+    setModal(true)
+  }
 
 
   const buscarEmocoes = async () => {
@@ -22,13 +37,22 @@ export default function LinhaEmocoes() {
   }
 
   return (
-    <div className="grid grid-cols-7 gap-4 py-5 content-center justify-items-center">
-      {emocoes && emocoes.map((emocao) => (
-        emocao.descricao == "FELIZ" ? <span key={emocao.id} className="text-5xl">{"\u{1F622}"}</span> :
-        emocao.descricao == "MEIOTERMO" ? <span key={emocao.id} className="text-5xl">{"\u{1F610}"}</span> : 
-        <span key={emocao.id} className="text-5xl">{"\u{1F60A}"}</span>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-7 gap-4 py-5 content-center justify-items-center">
+        {emocoes && emocoes.map((emocao) => (
+          emocao.descricao == "FELIZ" ? <span key={emocao.id} onClick={() => onClickEmocao(emocao, "\u{1F60A}")} className="text-5xl hover:scale-110 hover:cursor-pointer duration-500 ease-in-out" >{"\u{1F60A}"}</span> :
+          emocao.descricao == "MEIOTERMO" ? <span key={emocao.id} className="text-5xl hover:scale-110 hover:cursor-pointer duration-500 ease-in-out" onClick={() => onClickEmocao(emocao, "\u{1F610}")}>{"\u{1F610}"}</span> : 
+            <span key={emocao.id} className="text-5xl hover:scale-110 hover:cursor-pointer duration-500 ease-in-out" onClick={() => onClickEmocao(emocao, "\u{1F622}")}>{"\u{1F622}"}</span>
+        ))}
+      </div>
+      <div id="modal-root"></div>
+      {modal && (
+        <ModalInfoEmocao info={infoModal} emoji={emoji} onClose={
+          () => {setModal(false) 
+          console.log(infoModal)}
+        }/>
+      )}
+    </>
       // <div>
       //   <div className="grid grid-cols-7 gap-4 py-5 content-center justify-items-center">
       //   <div className="w-4 md:w-8 rounded-full h-4 md:h-8 bg-verde2">{"\u{1F622}"}</div>
---
+++ src/pages/components/Usuario/PaginaInicial/Calendario/ModalInfoEmocao.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { IoCloseSharp } from "react-icons/io5";
+import { createRoot } from 'react-dom/client';
+import Emocao from "../Emocoes/Emocao";
+
+const ModalInfoEmocao = ({ onClose, title, info, emoji }) => {
+  const handleCloseClick = (e) => {
+    e.preventDefault();
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+
+    console.log(info)
+
+    const modalContent = (
+      <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50" >
+        <div className="xs:w-[80%] xs:h-[80%] md:w-[500px]  md:h-[600px] bg-white rounded-lg p-4">
+          <div className="flex justify-end text-2xl items-center">
+            <div
+              className="h-20 w-screen
+             text-center flex justify-center items-center "
+            >
+              <h2 className="font-calistoga w-full sm:text-3xl">
+                Emoção do Dia
+              </h2>
+            </div>
+            <button
+              className="absolute w-auto h-auto p-0  sm:hidden"
+              onClick={handleCloseClick}
+            >
+              <IoCloseSharp size={20} />
+            </button>
+  
+            <button
+              className="sm:flex sm:absolute w-auto h-auto p-0 py-2 hidden"
+              onClick={handleCloseClick}
+            >
+              <IoCloseSharp size={45} />
+            </button>
+          </div>
+          {title && <h1 className="text-xl font-bold">{title}</h1>}
+          <div className="w-full h-full">
+            <h1 className="text-center text-xl font-nunito">{emoji}</h1>
+            <h1 className="text-center text-xl font-nunito">{info[1]}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  
+    const portalElement = document.getElementById("modal-root");
+    const root = createRoot(portalElement);
+  
+    root.render(modalContent);
+  
+    return () => {
+      root.unmount();
+    };
+  },)
+
+};
+
+export default ModalInfoEmocao;
