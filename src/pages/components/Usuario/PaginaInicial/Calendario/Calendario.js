b891162bcbbd3a73b93f982f02d746dcee343e60
vitorvhsilva <dndragonbr@gmail.com>
2024-06-14 11:08:52 -0300

    @vitorvhsilva Responsividade dos emoções

--- src/pages/components/Usuario/PaginaInicial/Calendario/Calendario.js
+++ src/pages/components/Usuario/PaginaInicial/Calendario/Calendario.js
@@ -5,7 +5,7 @@ export default function Calendario({titulo, emocoes}) {
   return (
     <div className="flex flex-col justify-center items-center pb-20">
       <h2 className="text-3xl md:text-5xl font-nunito text-amarelo2 py-10">{titulo}</h2>
-      <div className="w-[80%] bg-branco border-solid border-2 border-verde2/50 rounded-lg">
+      <div className="md:w-[80%] w-[95%] bg-branco border-solid border-2 border-verde2/50 rounded-lg">
         {emocoes ? 
         <LinhaEmocoes/> : <LinhaCalendario/>
         }      
