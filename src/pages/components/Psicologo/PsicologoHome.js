158e8fa2ff01b21008b950d1d3192e97651f723d
LuizHms55 <lhmsousa@gmail.com>
2024-06-14 16:10:33 -0300

    @LuizHms55 Home psicologo finalizada

--- src/pages/components/Psicologo/PsicologoHome.js
+++ src/pages/components/Psicologo/PsicologoHome.js
@@ -5,7 +5,7 @@ export default function PsicologoHome(){
         <div className="w-[100%] h-auto flex items-center  ">
     <div className="flex flex-col justify-center items-center h-screen w-[40%] mt-10">
         <h1 className="font-calistoga text-7xl w-full text-center">Olá,Nome do profissional</h1>
-        <p className="font-nunito text-xl font-semibold mt-6">Aqui estão algumas coisas que podem te interessar.</p>
+        <p className="font-nunito text-4xl font-medium mt-6">Aqui estão algumas coisas <br/>que podem te interessar.</p>
     </div>
     <div className="flex flex-col justify-between items-center h-[95%] w-[60%] px-8 gap-3">
         <section className="w-[80%] h-[33%] flex flex-col gap-6 mb-8">
@@ -49,21 +49,21 @@ export default function PsicologoHome(){
                 <p>Atualizações do Blog</p>
             </div>
             <div className="flex justify-center"> 
-                <div className="flex flex-row items-center text-center justify-around gap-4">
+                <div className="flex flex-row items-center text-center justify-around gap-4 font-medium">
                        <Image width={30} height={30} src="/assets/psicologo/iconfav.png"></Image>
                        <p>Seu perfil foi favoritado (n°) vezes</p>
                 </div>
             </div>
 
             <div className="flex justify-center items-center px-6 mt-2 mb-2"> 
-                <div className="flex flex-row items-center text-center justify-around gap-4">
+                <div className="flex flex-row items-center text-center justify-around gap-4 font-medium">
                        <Image width={30} height={30} src="/assets/psicologo/iconanalise.png"></Image>
                        <p>Seu perfil foi favoritado (n°) vezes</p>
                 </div>
             </div>
 
-            <div className="flex justify-center items-center font-nunito font-bold text-xl">
-                <p>Veja os dados completos em Estatisticas</p>
+            <div className="flex justify-center items-center text-center font-nunito font-bold text-xl">
+                <p>Veja os dados completos <br/>em <span className="text-roxo">Estatisticas</span></p>
             </div>
         </section>
        
