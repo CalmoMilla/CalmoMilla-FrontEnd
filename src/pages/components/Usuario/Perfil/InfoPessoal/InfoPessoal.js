1b730b140ea3a6dcd21e94532f69c117502837e8
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-15 20:07:24 -0300

    @selingindev  ~mudança visual e  responsivo na infopessoal

--- src/pages/components/Usuario/Perfil/InfoPessoal/InfoPessoal.js
+++ src/pages/components/Usuario/Perfil/InfoPessoal/InfoPessoal.js
@@ -1,40 +1,60 @@
-"use client"
-import CampoPessoal from "../CampoPessoal"
+"use client";
+import CampoPessoal from "../CampoPessoal";
 import { useRouter } from "next/navigation";
 
 export default function InfoPessoal(props) {
   const router = useRouter();
 
   const handleDeslogar = () => {
-    localStorage.removeItem("token")
-    localStorage.removeItem("usuario")
-    router.push("/login")
-  }
+    localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+    router.push("/login");
+  };
 
-  let dataNasc = props.usuario ? props.usuario.dataNasc[2] + "/" + props.usuario.dataNasc[1] + "/" + 
-  props.usuario.dataNasc[0] : ""
-  let genero = props.usuario ? props.usuario.genero : ""
+  let dataNasc = props.usuario
+    ? props.usuario.dataNasc[2] +
+      "/" +
+      props.usuario.dataNasc[1] +
+      "/" +
+      props.usuario.dataNasc[0]
+    : "";
+  let genero = props.usuario ? props.usuario.genero : "";
   if (genero == "m") {
-    genero = "Masculino"
+    genero = "Masculino";
   } else if (genero == "f") {
-    genero = "Feminino"
+    genero = "Feminino";
   } else {
-    genero = "Outros"
+    genero = "Outros";
   }
 
   return (
     <>
-      <h2 className="text-5xl text-amarelo1 font-calistoga text-center py-5">Informações Pessoais</h2>
-      <div className="flex justify-center flex-col items-center py-5">     
-        <CampoPessoal texto1={"Nome"} texto2={props.usuario ? props.usuario.nome : ""}/>
-        <CampoPessoal texto1={"Data de nascimento"} texto2={props.usuario ? dataNasc : ""}/>
-        <CampoPessoal texto1={"Gênero"} texto2={props.usuario ? genero : ""}/>
+      <h2 className="text-5xl text-amarelo2 font-calistoga text-center py-5">
+        Informações Pessoais
+      </h2>
+      <div className="flex justify-center flex-col items-center py-5">
+        <CampoPessoal
+          texto1={"Nome"}
+          texto2={props.usuario ? props.usuario.nome : ""}
+        />
+        <CampoPessoal
+          texto1={"Data de nascimento"}
+          texto2={props.usuario ? dataNasc : ""}
+        />
+        <CampoPessoal texto1={"Gênero"} texto2={props.usuario ? genero : ""} />
         <div className="w-[80%] flex justify-between items-center py-5">
-          <h2 className="text-2xl text-amarelo1 py-5">Mudar informações da conta</h2>
+          <h2 className="text-2xl text-amarelo2 py-5">
+            Mudar informações da conta
+          </h2>
           <h2 className=""></h2>
         </div>
-        <button className="w-auto px-5 py-3 mx-auto bg-amarelo1 hover:bg-amarelo2 duration-500 ease-in-out p-0 rounded-full text-white text-xl" onClick={handleDeslogar}>Deslogar</button>
+        <button
+          className=" xs:w-[250px] sm:w-[300px] px-5 py-3 mx-auto xs:bg-amarelo2 lg:bg-amarelo1  hover:bg-amarelo2 duration-500 ease-in-out p-0 rounded-full text-white text-xl"
+          onClick={handleDeslogar}
+        >
+          DESLOGAR
+        </button>
       </div>
     </>
-  )
+  );
 }
