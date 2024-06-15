8d7f8601be3211ae4fef14649f82ecbe6d4337e2
vitorvhsilva <dndragonbr@gmail.com>
2024-06-14 23:08:05 -0300

    @vitorvhsilva Arrumando Deploy no Vercel

--- src/pages/components/Psicologo/Recursos/BeneficioJogo.js
+++ src/pages/components/Psicologo/Recursos/BeneficioJogo.js
@@ -6,14 +6,14 @@ export default function BeneficioJogo({funcao, jogo}) {
   return (
     <div className="w-full h-fit relative">
       <div className="w-[90%] md:w-[70%] h-fit relative mx-auto shadow-2xl rounded-xl">
-        <h1 className="text-center text-7xl font-calistoga">{jogo[1]}</h1>
+        <h1 className="text-center text-7xl font-calistoga">{jogo && jogo[1]}</h1>
         <h2 className="text-center text-3xl font-nunito">Sua história e benefícios</h2>
-        <Image src={jogo[2]} alt={"Imagem do Benefício"} width={800} height={800} className="mx-auto pt-12"/>
+        <Image src={jogo && jogo[2]} alt={"Imagem do Benefício"} width={800} height={800} className="mx-auto pt-12"/>
         <p className="w-[70%] font-nunito text-xl mx-auto py-10">Lorem ipsum dolor sit amet. Eos laboriosam nesciunt vel aliquam harum est voluptates natus est officiis voluptas. Qui eveniet rerum ut officia dolores et odit autem ad dicta quia nam fuga nihil et internos reiciendis est asperiores aperiam! Ut blanditiis ducimus est libero pariatur sit saepe quia hic sunt doloribus. Et assumenda repudiandae sed necessitatibus voluptas et officia temporibus quo aliquid ratione At officiis officiis. Sit magni tempora et architecto dolore eum distinctio quasi ut laboriosam totam eos dignissimos tempora non natus aliquam! Ut laudantium itaque est architecto debitis sit accusantium beatae sed nisi iure sed consequatur iure quo quis voluptates ea nemo quae. Sed animi reiciendis ex possimus unde sed veniam repudiandae.
         Qui excepturi dolorum ut omnis dolor non labore deserunt est quos quia vel quod debitis. Qui repellat delectus qui similique culpa sed voluptas illum vel unde consequatur aut officia corporis hic dolor quas vel molestias fuga. Qui cumque deleniti a harum adipisci hic placeat iste est quibusdam magni 33 aspernatur galisum? A autem porro eos voluptate aperiam qui consequatur labore non sunt ipsum est molestiae repellat ab vero ipsam. Aut expedita neque ad accusamus eaque aut cupiditate rerum.
         Eum incidunt Quis ex debitis suscipit est consequatur modi aut voluptatem perspiciatis sed minima possimus eos atque aspernatur! Et magnam fugit eos repellat voluptates et quas molestiae. Qui cumque sint aut voluptatem maiores ex eius nisi qui consectetur sunt At quia praesentium? Est consequatur rerum et repellendus excepturi eum neque minus ea quis voluptatem. Eos dolor maiores aut accusamus obcaecati aut commodi reprehenderit aut maxime consectetur eos aliquam cupiditate ab molestiae provident a cumque debitis.</p>
       </div>
-      <p className="text-center text-xl font-bold font-nunito py-5">{jogo[3]}</p>
+      <p className="text-center text-xl font-bold font-nunito py-5">{jogo && jogo[3]}</p>
       <IoIosArrowDropleftCircle className="absolute top-5 left-7 text-5xl md:top-5 md:left-10 md:text-7xl text-amarelo1 hover:text-amarelo2 duration-500 ease-in-out hover:cursor-pointer hover:scale-110" onClick={funcao} />
     </div>
   )
--- src/pages/components/Usuario/Psicologo/Psicologos/Psicologos.js
+++ src/pages/components/Usuario/Psicologo/Psicologos/Psicologos.js
@@ -8,7 +8,7 @@ export default function Psicologos({data}) {
         <div className="w-[60%] h-fit">
             <h1 className="text-verdeagua text-7xl font-calistoga text-center drop-shadow-2xl">Psicologos</h1>
             <div className="h-fit w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8 py-10">
-                {psicologos.map((psi) => (
+                {psicologos && psicologos.map((psi) => (
                     <Psicologo key={psi.id} nome={psi.nome} idade={psi.idade} cidade={psi.cidade}/>
                 ))}        
             </div>
