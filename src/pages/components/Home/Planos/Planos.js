ab96b5ca9e0f6d0e810231262087243aac74fff4
vitorvhsilva <dndragonbr@gmail.com>
2024-06-17 17:34:04 -0300

    @vitorvhsilva @selingindev Arrumando a responsividade do Planos

--- src/pages/components/Home/Planos/Planos.js
+++ src/pages/components/Home/Planos/Planos.js
@@ -4,8 +4,8 @@ import Plano from "./Plano";
 export default function Planos() {
   return (
     <div className="bg-branco p-5">
-      <h2 className="text-verdeagua text-7xl font-calistoga text-center py-5 ">Planos Disponíveis</h2>
-      <h2 className="text-preto text-5xl font-calistoga text-center py-5 ">Usuários</h2>
+      <h2 className="text-verdeagua text-5xl  md:text-7xl font-calistoga text-center py-5 ">Planos Disponíveis</h2>
+      <h2 className="text-preto text-3xl md:text-5xl font-calistoga text-center py-5 ">Usuários</h2>
       <div className="sm:flex-col lg:flex lg:flex-row justify-around pb-12">
         <Plano preco={"R$15.00"} plano={"Mensal"}/>
         <Plano preco={"R$30.00"} plano={"Vitalício"}/>
