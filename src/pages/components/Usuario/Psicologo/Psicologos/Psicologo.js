837f932680bcfe99fdc4e12b92625dce55f1490c
vitorvhsilva <dndragonbr@gmail.com>
2024-06-21 00:28:56 -0300

    @vitorvhsilva Cores do Psicólogo

--- src/pages/components/Usuario/Psicologo/Psicologos/Psicologo.js
+++ src/pages/components/Usuario/Psicologo/Psicologos/Psicologo.js
@@ -2,6 +2,16 @@ import Image from "next/image"
 
 export default function Psicologo({psi, guardarInfo, funcao}) {
 
+  const corPsicologo = () => {
+    const cores = ["bg-amarelo1", "bg-verde2", "bg-roxo"]; // Array de cores disponíveis
+
+    // Gera um índice aleatório
+    const indiceAleatorio = Math.floor(Math.random() * cores.length);
+  
+    // Retorna a cor aleatória
+    return cores[indiceAleatorio];
+  }
+
   function abbreviateName(name, maxLength = 15) {
     if (!name || name.length <= maxLength) {
       return name
@@ -42,7 +52,7 @@ export default function Psicologo({psi, guardarInfo, funcao}) {
 
   return (
     <div className="w-[90%] h-full shadow-2xl rounded-xl mx-auto hover:scale-110 hover:cursor-pointer duration-500 ease-in-out" onClick={onClickPsicologo}>
-      <div className="w-full h-[55%] bg-preto/20 flex justify-start items-end p-5 rounded-t-lg">
+      <div className={`w-full h-[55%] ${corPsicologo()}  flex justify-start items-end p-5 rounded-t-lg`}>
           <Image src={psi && psi.foto} alt={"Imagem do Usuario"} width={100} height={100} className="rounded-full"/>
       </div>
       <div className="w-full h-[45%] py-2 px-5">
