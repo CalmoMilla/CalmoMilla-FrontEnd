7be8e169f340b06b8d89462a0708c223cf34a033
vitorvhsilva <dndragonbr@gmail.com>
2024-06-23 11:19:20 -0300

    @vitorvhsilva Login com Google feito

--- src/pages/components/Login/Login.js
+++ src/pages/components/Login/Login.js
@@ -1,23 +1,25 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState  } from "react";
 import Image from "next/image";
 import CadastroGoogle from "../Cadastro/CadastroGoogle";
 import Modal from "./modalAlterarSenha";
 import EsqueciSenha from "./EsqueciSenha";
-
+import {
+  LoginComGoogle,
+  LoginUsuario,
+} from "@/pages/api/usuario/UsuarioService";
 import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
 import { signIn, signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import LoginUser from "./LoginUser";
-import LoginPsico from "./LoginPsico";
 
 export default function Login() {
   const [showCadastro, setShowCadastro] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
   const [alterarSenha, setAlterarSenha] = useState(false);
-  const [AlterarLogin, setAlterarLogin] = useState(true);
+
+  const [email, setEmail] = useState("");
 
   const router = useRouter();
 
@@ -26,15 +28,16 @@ export default function Login() {
     setTimeout(() => {
       setShowCadastro(true);
       setIsAnimating(false);
-    }, 1100);
+    }, 1100); // 1000ms é a duração da animação
   };
 
   const session = useSession();
 
   useEffect(() => {
     if (session && session.status === "authenticated") {
-      setEmail(session.data.user.email);
-      onAuth();
+      setEmail(session.data.user.email)
+      onAuth()
+      setTimeout(() => {window.location.reload()}, 1000)
     }
   }, [session]);
 
@@ -44,6 +47,34 @@ export default function Login() {
       router.push("/usuario");
     }
   });
+
+  async function onAuth() {
+    let login = {
+      email: session.data.user.email,
+    };
+    LoginComGoogle(login, "auth/login/google");
+    const tokenUser = localStorage.getItem("token");
+    if (tokenUser != "" && tokenUser != undefined && tokenUser != null) {
+      router.push("/usuario");
+    }
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    let login = {
+      email: data.get("email"),
+      senha: data.get("senha"),
+    };
+
+    await LoginUsuario(login, "auth/login");
+    // await LoginPsicologo(login, "auth/login");
+    const tokenUser = localStorage.getItem("token");
+    if (tokenUser != "" && tokenUser != undefined && tokenUser != null) {
+      router.push("/usuario");
+    }
+  }
 
   if (showCadastro) {
     return <CadastroGoogle />;
@@ -101,17 +132,11 @@ export default function Login() {
           />
         </div>
         <div className="flex justify-center items-center w-full xs:h-auto lg:h-[65%] lg:w-[100%] ">
-          <div className="lg:h-full xs:h-screen w-full m-auto grid-cols-2 border-black justify-center items-centers">
+          <div className="md:h-full xs:h-screen w-full m-auto grid-cols-2 border-black justify-center items-centers">
             <div className="lg:w-[60%] h-20 m-auto flex-col lg:text-start xs:w-full">
-              {AlterarLogin ? (
-                <h2 className="h-1/2 xs:text-center xs:text-xl sm:text-2xl xl:text-3xl lg:text-start font-calistoga">
-                  Login <span className="text-amarelo2"> Usúario</span>
-                </h2>
-              ) : (
-                <h2 className="h-1/2 xs:text-center xs:text-xl sm:text-2xl xl:text-3xl lg:text-start font-calistoga">
-                  Login <span className="text-amarelo2 "> Psicólogo</span>
-                </h2>
-              )}
+              <h2 className="h-1/2 xs:text-center xs:text-xl sm:text-2xl xl:text-3xl lg:text-start font-calistoga">
+                Login
+              </h2>
               <p className="font-nunito font-bold h-1/2 xs:text-center lg:text-start">
                 Se você não tiver uma conta,
                 <br />
@@ -125,24 +150,57 @@ export default function Login() {
                 aqui!
               </p>
             </div>
-            {AlterarLogin ? <LoginUser /> : <LoginPsico />}
+            <form
+              className="md:w-[85%] flex-col text-start flex h-[50%] items-start mx-auto mt-4 justify-around gap-1 sm:w-full sm:px-0"
+              action="post"
+              onSubmit={onSubmit}
+            >
+              <div className="grid-cols-2 flex-col flex justify-center border-b-2 w-[70%] border-black xs:m-auto">
+                <label className="flex text-amarelo2" htmlFor="email">
+                  Email{" "}
+                </label>
+                <input
+                  className="border-none border-b-2 font-nunito placeholder:text-black text-sm"
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="Coloque seu Email."
+                  required value={email} onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className="grid-cols-2 flex-col flex justify-center border-b-2 w-[70%] border-black xs:m-auto">
+                <label htmlFor="senha" className="flex text-amarelo2">
+                  Senha{" "}
+                </label>
+                <input
+                  type="password"
+                  id="senha"
+                  name="senha"
+                  className="border-none border-b-2 font-nunito placeholder:text-black text-sm"
+                  placeholder="Escreva sua senha."
+                  
+                />
+              </div>
+              <div className="flex w-[70%] justify-center xs:m-auto flex-col items-center">
+                <button
+                  type="submit"
+                  className="w-full h-10 bg-amarelo2 justify-center p-0 mt-4 rounded-full text-white"
+                >
+                  Entrar
+                </button>
 
-            <div className="xs:mx-auto w-[60%] h-auto m-auto xs:text-start gap-10">
-              {AlterarLogin ? (
-                <button
-                  onClick={() => setAlterarLogin(false)}
-                  className="w-full font-nunito tracking-normal cursor-pointer p-0 mt-2 mb-4 font-bold text-start "
+                <p className="font-nunito text-lg lg:hidden my-2 xs:flex text-center">
+                  Ou
+                </p>
+                <div
+                  className="bg-branco my-2 w-full lg:w-[70%]   h-8 flex justify-center items-center border rounded-3xl border-preto/50 mx-auto hover:cursor-pointer xs:flex xl:hidden "
+                  onClick={() => signIn("google")}
                 >
-                  Sou um<span className="text-amarelo2"> Psicólogo </span>!
-                </button>
-              ) : (
-                <button
-                  onClick={() => setAlterarLogin(true)}
-                  className="w-full font-nunito tracking-normal cursor-pointer p-0 mt-2 mb-4 font-bold text-start "
-                >
-                  Sou um<span className="text-amarelo2"> Usúario </span>!
-                </button>
-              )}
+                  <FcGoogle className="text-3xl my-4" />
+                </div>
+              </div>
+            </form>
+            <div className="xs:mx-auto w-[60%] h-auto m-auto mt-4 xs:text-start gap-10">
               <div className="w-full h-5 flex items-center mb-4">
                 <input
                   name="continuar"
@@ -157,7 +215,6 @@ export default function Login() {
                   Continuar conectado.
                 </label>
               </div>
-
               <button
                 onClick={() => setAlterarSenha(true)}
                 className="w-full font-nunito tracking-normal cursor-pointer font-bold text-start p-0"
