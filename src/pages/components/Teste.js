ee258c423383e00b1853d3e242cd64fc41fb87ea
vitorvhsilva <dndragonbr@gmail.com>
2024-06-23 10:49:26 -0300

    @vitorvhsilva Recursos com descrição

--- src/pages/components/Teste.js
+++ src/pages/components/Teste.js
@@ -1,10 +1,11 @@
 "use client";
 
 import SobreNos from "./Home/SobreNos/SobreNos";
+import Recursos from "./Psicologo/Recursos/Recursos";
 import EstatisticasPsicologo from "./Usuario/Psicologo/EstatisticasPsicologo/EstatisticasPsicologo";
 import InfoPsicologo from "./Usuario/Psicologo/InfoPsicologo/InfoPsicologo";
 import PaginaPsicologo from "./Usuario/Psicologo/PaginaPsicologo";
 
 export default function Teste() {
-  return <PaginaPsicologo />;
+  return <Recursos />;
 }
