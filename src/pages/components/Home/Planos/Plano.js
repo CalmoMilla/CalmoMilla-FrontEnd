902e101a766760052b33ef88e51b4286acf7f62a
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-24 20:31:46 -0300

    @selingindev ~ajuste identidade visual e responsivo nos planos

--- src/pages/components/Home/Planos/Plano.js
+++ src/pages/components/Home/Planos/Plano.js
@@ -4,24 +4,31 @@ export default function Plano(props) {
   return (
     <div className="flex flex-col justify-center items-center ">
       <div className="xs:mb-20 xs:mt-10 lg:mb-0 xs:w-[80%] sm:w-auto shadow-2xl rounded-2xl">
-        <div className="w-fill h-fill flex flex-col px-12 py-5 items-center justify-center bg-branco shadow-2xl rounded-2xl">
-          <h3 className="text-center w-full text-5xl font-calistoga text-preto">{props.plano}</h3>
-          <div className="w-full h-fit flex flex-col gap-4 items-center justify-around py-5" >
-            <div className="w-fit h-fit flex items-center justify-around" >
+        <div className="w-fill h-fill flex flex-col sm:px-12 py-5 items-center justify-center bg-branco shadow-2xl rounded-2xl">
+          <h3 className="text-center w-full text-5xl font-calistoga text-preto">
+            {props.plano}
+          </h3>
+          <div className="w-full h-fit flex flex-col gap-4 items-center justify-around py-5">
+            <div className="w-fit h-fit flex items-center justify-around">
               <FaRegCheckCircle className="text-2xl text-verde2" />
               <p className="font-nunito text-xl">Sem anúncio</p>
             </div>
-            {props.psicologo && 
-              <div className="w-fit h-fit flex items-center justify-around" >
-              <FaRegCheckCircle className="text-2xl text-verde2" />
-              <p className="font-nunito text-md">Acesso a estatísticas</p>
-            </div>
-            }
+            {props.psicologo && (
+              <div className="w-fit h-fit flex items-center justify-around">
+                <FaRegCheckCircle className="text-2xl text-verde2" />
+                <p className="font-nunito text-md">Acesso a estatísticas</p>
+              </div>
+            )}
           </div>
-          <p className="w-full text-3xl text-center pt-20 pb-10 font-nunito">Apenas <br />{props.preco}</p>
-          <button className="bg-verdeagua/50 hover:bg-verdeagua text-branco mx-auto block text-2xl px-10 py-3 font-nunito duration-500 ease-in-out rounded-2xl">Assinar</button>
+          <p className="w-full text-3xl text-center pt-20 pb-10 font-nunito">
+            Apenas <br />
+            {props.preco}
+          </p>
+          <button className="bg-verdeagua/50 hover:bg-verdeagua text-branco mx-auto block text-2xl px-10 py-3 font-nunito duration-500 ease-in-out rounded-2xl">
+            Assinar
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/components/Home/Planos/Planos.js
+++ src/pages/components/Home/Planos/Planos.js
@@ -4,15 +4,27 @@ import Plano from "./Plano";
 export default function Planos() {
   return (
     <div className="bg-branco p-5">
-      <h2 className="text-verdeagua text-5xl  md:text-7xl font-calistoga text-center py-5 ">Planos Disponíveis</h2>
-      <h2 className="text-preto text-3xl md:text-5xl font-calistoga text-center py-5 ">Usuários</h2>
+      <h2 className="text-verdeagua text-5xl  md:text-7xl font-calistoga text-center py-5 ">
+        Planos Disponíveis
+      </h2>
+      <h2 className="text-preto text-3xl text-5xl font-calistoga text-center py-5 ">
+        Flores
+      </h2>
+      <span className=" w-full flex  justify-center text-center TEXT-XS text-amarelo2 font-nunito">
+        (USUÁRIO COMUM)
+      </span>
       <div className="sm:flex-col lg:flex lg:flex-row justify-around pb-12">
-        <Plano preco={"R$15.00"} plano={"Mensal"}/>
-        <Plano preco={"R$30.00"} plano={"Vitalício"}/>
+        <Plano preco={"R$15.00"} plano={"Mensal"} />
+        <Plano preco={"R$30.00"} plano={"Vitalício"} />
       </div>
       <div className="w-[80%] h-1 bg-preto/20 mx-auto"></div>
-      <h2 className="text-preto text-5xl font-calistoga text-center py-10">Psicólogos</h2>
-      <Plano preco={"R$15.00"} plano={"Vitalício"} psicologo={true}/>
+      <h2 className="text-preto text-5xl font-calistoga text-center py-10">
+        Jardineiros
+      </h2>
+      <span className=" w-full flex  justify-center text-center TEXT-XS text-amarelo2 font-nunito">
+        (PROFISSIONAL DA SAÚDE)
+      </span>
+      <Plano preco={"R$15.00"} plano={"Vitalício"} psicologo={true} />
     </div>
-  )
+  );
 }
