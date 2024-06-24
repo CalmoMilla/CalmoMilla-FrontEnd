a18f9d517a655e8266174a6b49f9cc50da4d985f
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-24 18:24:53 -0300

    @selingindev ~correção responsivo no tablet pagina inicial

--- src/pages/components/Home/PaginaInicial/PaginaInicial.js
+++ src/pages/components/Home/PaginaInicial/PaginaInicial.js
@@ -1,28 +1,45 @@
-import Image from "next/image"
+import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 
 export default function PaginaInicial() {
-  const { t } = useTranslation()
-  
+  const { t } = useTranslation();
+
   return (
     <>
-      <div className="w-full xl:h-[100vh] lg:h-[120vh] md:h-[150vh] h-[100vh] bg-cover bg-no-repeat bg-[url('/assets/home/homepage/fundo2.jpg')] md:bg-[url('/assets/home/homepage/fundo3.jpg')]">
-        <div className="bg-preto/50 w-full xl:h-[100vh] lg:h-[120vh] md:h-[150vh] h-[100vh] flex justify-center m-0" >
+      <div className="w-full xl:h-[100vh] lg:h-[120vh]  md:h-[80vh] h-screen bg-cover bg-no-repeat bg-[url('/assets/home/homepage/fundo2.jpg')] md:bg-[url('/assets/home/homepage/fundo3.jpg')]">
+        <div className="bg-preto/50 w-full xl:h-[100vh] lg:h-[120vh] md:h-[80vh] h-screen flex justify-center m-0">
           <div className="w-[full] flex xl:justify-between xl:gap-20 items-center lg:flex-row flex-col pt-40">
             <div>
-              <h1 className="font-calistoga xs:pb-8 xs:text-6xl md:text-[8rem] text-branco">Calmo<span className="text-amarelo1">Milla</span></h1>
-              <h2 className="font-calistoga xs:text-4xl md:text-6xl text-branco"><span className="text-amarelo1">{t("paginaInicial1")}</span> {t("paginaInicial2")}<br />{t("paginaInicial3")}<br /> <span className="text-amarelo1">{t("paginaInicial4")}</span></h2>
+              <h1 className="font-calistoga xs:pb-8 xs:text-6xl md:text-[8rem] text-branco">
+                Calmo<span className="text-amarelo1">Milla</span>
+              </h1>
+              <h2 className="font-calistoga xs:text-4xl md:text-6xl text-branco">
+                <span className="text-amarelo1">{t("paginaInicial1")}</span>{" "}
+                {t("paginaInicial2")}
+                <br />
+                {t("paginaInicial3")}
+                <br />{" "}
+                <span className="text-amarelo1">{t("paginaInicial4")}</span>
+              </h2>
               <Link href={"/login"} className="w-fit">
-                <button className="text-xl lg:text-3xl xs:text-3xl bg-branco mt-20 px-10 py-4 rounded-full hover:bg-amarelo2 hover:text-branco transition duration-300 ease-out">{t("paginaInicialBotao")}</button>
+                <button className="text-xl lg:text-3xl xs:text-3xl bg-branco mt-20 px-10 py-4 rounded-full hover:bg-amarelo2 hover:text-branco transition duration-300 ease-out">
+                  {t("paginaInicialBotao")}
+                </button>
               </Link>
             </div>
             <div>
-            <Image className="lg:block hidden m-0 " width={280} height={280} src="/assets/millazinha/milla02.png" alt=""/>
+              <Image
+                className="lg:block hidden m-0 "
+                width={280}
+                height={280}
+                src="/assets/millazinha/milla02.png"
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
