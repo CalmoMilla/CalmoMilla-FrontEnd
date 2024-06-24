a9de0a44dd5e9ef4ec01ae4f0e56e506cd78a02f
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-24 18:52:29 -0300

    @selingindev ~header da pagina sem login corrigido

--- src/pages/components/Header/Header.js
+++ src/pages/components/Header/Header.js
@@ -13,22 +13,24 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    setIsOpen((prevState) => !prevState);
+    if (window.innerWidth < 1280) {
+      setIsOpen((prevState) => !prevState);
+    }
   };
 
   const [logado, setLogado] = useState(false);
   const [usuario, setUsuario] = useState(null);
-  const [foto, setFoto] = useState(null)
+  const [foto, setFoto] = useState(null);
 
   useEffect(() => {
     let usuarioStorage = localStorage.getItem("usuario");
     if (usuarioStorage != null) {
-      usuarioStorage = JSON.parse(usuarioStorage)
-      console.log(usuarioStorage)
+      usuarioStorage = JSON.parse(usuarioStorage);
+      console.log(usuarioStorage);
       setUsuario(usuarioStorage ? usuarioStorage : null);
-      setFoto(usuarioStorage.foto)
+      setFoto(usuarioStorage.foto);
     }
-  },[]);
+  }, []);
 
   useEffect(() => {
     if (usuario != null) {
@@ -39,25 +41,29 @@ export default function Header() {
   return (
     <>
       <header className="bg-amarelo1 py-3 z-50 w-screen fixed">
-        <nav className={`flex justify-between items-center w-[92%] mx-auto z-50${
-                  isOpen ? "   z-50"  : " "
-                } z-50`}>
+        <nav
+          className={`flex justify-between items-center w-[92%] mx-auto z-50${
+            isOpen ? "   z-50" : " "
+          } z-50`}
+        >
           <Link href={`${logado ? "/usuario" : "/"}`} className="w-fit">
             <Image width={70} height={70} src="/assets/logo.png" alt="..." />
           </Link>
           {!logado ? (
             <>
-          
               <div
                 className={`bg-amarelo1 nav-links  xl:flex xl:static fixed lg:min-h-fit min-h-[60vh] left-0 top-[-100%] xl:w-auto w-full flex items-center px-5 flex-re ${
-                  isOpen ? "xs:top-[100%] absolute duration-300  h-96  items-center z-10 "  : "  h-12"
-                } z-0`} 
+                  isOpen
+                    ? "xs:top-[100%] absolute duration-300  h-96  items-center z-10 "
+                    : "  h-12"
+                } z-0`}
               >
-            
-                <ul className={`flex xl:flex-row flex-col sm:items-center   xs:flex xl:flex xl:gap-[4vw] gap-8  ${
-                  isOpen ? "w-full " : " "
-                }`}>
-                       <button className="bg-branco px-8 py-3 rounded-full  md:hidden xs:w-[60%] md:w-auto hover:bg-amarelo2 hover:text-branco transition duration-300 ease-out text-xl xl:text-2xl">
+                <ul
+                  className={`flex xl:flex-row flex-col sm:items-center   xs:flex xl:flex xl:gap-[4vw] gap-8  ${
+                    isOpen ? "w-full " : " "
+                  }`}
+                >
+                  <button className="bg-branco px-8 py-3 rounded-full  md:hidden xs:w-[60%] md:w-auto hover:bg-amarelo2 hover:text-branco transition duration-300 ease-out text-xl xl:text-2xl">
                     {t("common:headerEntrar")}
                   </button>
                   <li>
@@ -107,7 +113,7 @@ export default function Header() {
                   </li>
                 </ul>
               </div>
-                       
+
               <div className="flex items-center  gap-6">
                 <LanguageChanger />
                 <Link href={"/login"}>
@@ -132,15 +138,18 @@ export default function Header() {
             </>
           ) : (
             <>
-           <div
+              <div
                 className={`bg-amarelo1 nav-links  xl:flex lg:static fixed lg:min-h-fit min-h-[60vh] left-0 top-[-100%] lg:w-auto w-full flex items-center px-5 flex-re ${
-                  isOpen ? "xs:top-[100%] absolute duration-300  items-center z-10 "  : "  h-12"
-                } z-0`} 
+                  isOpen
+                    ? "xs:top-[100%] absolute duration-300  items-center z-10 "
+                    : "  h-12"
+                } z-0`}
               >
-            
-                <ul className={`flex lg:flex-row flex-col sm:items-center  xs:flex xl:flex :gap-[4vw] gap-8  ${
-                  isOpen ? "w-full " : " "
-                }`}>
+                <ul
+                  className={`flex lg:flex-row flex-col sm:items-center  xs:flex xl:flex :gap-[4vw] gap-8  ${
+                    isOpen ? "w-full " : " "
+                  }`}
+                >
                   <li>
                     <Link
                       href={"/usuario/jogos"}
@@ -186,11 +195,17 @@ export default function Header() {
                   className="w-fit p-3 hover:bg-amarelo2 rounded-full duration-500 
                 transition ease-in-out"
                 >
-                {foto ? 
-                  <Image width={50} height={50} src={foto} alt="..." className="rounded-full"/>
-                  :
-                  <FaUser className="text-3xl cursor-pointer text-branco" /> 
-                }
+                  {foto ? (
+                    <Image
+                      width={50}
+                      height={50}
+                      src={foto}
+                      alt="..."
+                      className="rounded-full"
+                    />
+                  ) : (
+                    <FaUser className="text-3xl cursor-pointer text-branco" />
+                  )}
                 </Link>
                 {isOpen ? (
                   <FaTimes
@@ -208,7 +223,6 @@ export default function Header() {
               </div>
             </>
           )}
-          
         </nav>
       </header>
     </>
