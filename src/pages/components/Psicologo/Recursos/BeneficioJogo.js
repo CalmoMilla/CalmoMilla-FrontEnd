59628cd13b5cbc4540a367085228947b839dbdbf
vitorvhsilva <dndragonbr@gmail.com>
2024-06-25 22:34:27 -0300

    @vitorvhsilva Recursos do psicologico

--- src/pages/components/Psicologo/Recursos/BeneficioJogo.js
+++ src/pages/components/Psicologo/Recursos/BeneficioJogo.js
@@ -8,7 +8,7 @@ export default function BeneficioJogo({funcao, jogo}) {
       <div className="w-[90%] md:w-[70%] h-fit relative mx-auto shadow-2xl rounded-xl">
         <h1 className="text-center text-7xl font-calistoga mt-10 pt-10">{jogo && jogo[1]}</h1>
         <h2 className="text-center text-3xl font-nunito">Sua história e benefícios</h2>
-        <Image src={jogo && jogo[2]} alt={"Imagem do Benefício"} width={800} height={800} className="mx-auto pt-12"/>
+        <Image src={jogo && jogo[2]} alt={"Imagem do Benefício"} width={800} height={800} className="mx-auto pt-12 rounded-xl"/>
         <p className="w-[70%] font-nunito text-xl mx-auto py-10">{jogo[4] && jogo[4]}</p>
       </div>
       {/* <p className="text-center text-xl font-bold font-nunito py-5">{jogo && jogo[3]}</p> */}
