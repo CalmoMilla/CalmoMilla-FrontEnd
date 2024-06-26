0c42f94637f3b4b99655de0f4a6ec5427f8c9cfc
vitorvhsilva <dndragonbr@gmail.com>
2024-06-25 23:03:29 -0300

    @vitorvhsilva Informações dos pacientes

--- src/pages/components/Psicologo/PacientesVinculados/InfoPaciente.js
+++ src/pages/components/Psicologo/PacientesVinculados/InfoPaciente.js
@@ -2,6 +2,24 @@ import Image from "next/image"
 import { IoIosClose } from "react-icons/io";
 
 export default function InfoPaciente({nome, dataNasc, endereco, onClose}) {
+
+  const anoNascimento = dataNasc && dataNasc[0];
+  const mesNascimento = dataNasc && dataNasc[1];
+  const diaNascimento = dataNasc && dataNasc[2];
+
+  const dataAtual = new Date();
+  const anoAtual = dataAtual.getFullYear();
+  const mesAtual = dataAtual.getMonth() + 1;
+  const diaAtual = dataAtual.getDate();
+
+  let diferencaAnos = anoAtual - anoNascimento;
+
+  if (mesAtual < mesNascimento || (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+    diferencaAnos--;
+  }
+
+  const idade = diferencaAnos;
+
   return (
     <div className="w-[15%] h-fit shadow-2xl py-5 rounded-xl flex flex-col justify-center items-center gap-8">
       <div className="w-full flex justify-end">
@@ -10,7 +28,7 @@ export default function InfoPaciente({nome, dataNasc, endereco, onClose}) {
       </div>
       <Image src={"https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"} alt={"Imagem do Usuario"} width={200} height={200} className="rounded-full"/>
       <h2 className="font-nunito text-3xl font-bold">{nome}</h2>
-      <h2 className="font-nunito text-xl font-bold">{dataNasc} anos</h2>
+      <h2 className="font-nunito text-xl font-bold">{idade} anos</h2>
       <h2 className="font-nunito text-xl font-bold">{endereco}</h2>
 
       <button className="font-nunito px-8 py-3 bg-amarelo1 hover:bg-amarelo2 duration-500 ease-in-out rounded-3xl">Abrir perfil</button>
--- src/pages/components/Psicologo/PacientesVinculados/PacienteVinculado.js
+++ src/pages/components/Psicologo/PacientesVinculados/PacienteVinculado.js
@@ -13,6 +13,23 @@ export default function PacienteVinculado({id, nome, dataNasc, endereco, handleC
 
   const nomeAbreviado = abbreviateName(nome);
 
+  const anoNascimento = dataNasc && dataNasc[0];
+  const mesNascimento = dataNasc && dataNasc[1];
+  const diaNascimento = dataNasc && dataNasc[2];
+
+  const dataAtual = new Date();
+  const anoAtual = dataAtual.getFullYear();
+  const mesAtual = dataAtual.getMonth() + 1;
+  const diaAtual = dataAtual.getDate();
+
+  let diferencaAnos = anoAtual - anoNascimento;
+
+  if (mesAtual < mesNascimento || (mesAtual === mesNascimento && diaAtual < diaNascimento)) {
+    diferencaAnos--;
+  }
+
+  const idade = diferencaAnos;
+
   return (
     <div className="w-[90%] h-full shadow-2xl rounded-xl mx-auto hover:scale-110 hover:cursor-pointer duration-500 ease-in-out" onClick={() => handleClick({id, nome, dataNasc, endereco})}>
       <div className="w-full h-[55%] bg-preto/20 flex justify-start items-end p-5 rounded-t-lg">
@@ -20,7 +37,7 @@ export default function PacienteVinculado({id, nome, dataNasc, endereco, handleC
       </div>
       <div className="w-full h-[45%] py-2 px-5">
         <h2 className={`font-nunito text-3xl`}>{nomeAbreviado}</h2>
-        <p className="font-nunito text-xl">{dataNasc}</p>
+        <p className="font-nunito text-xl">{idade} anos</p>
         <p className="font-nunito text-xl">{endereco}</p>
       </div>
     </div>
