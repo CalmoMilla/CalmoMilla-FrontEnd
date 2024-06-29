a5f081ccc261053c5b934e8392d93997a5998b07
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-29 15:20:33 -0300

    @selingindev finalizando responsivo seção relaxamento

--- src/pages/components/Usuario/Relaxamento/SelecaoSecaoRelaxamento.js
+++ src/pages/components/Usuario/Relaxamento/SelecaoSecaoRelaxamento.js
@@ -1,21 +1,42 @@
-import Image from "next/image"
-import { useTranslation } from "react-i18next"
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
 
 export default function SelecaoSecaoRelaxamento({ funcao }) {
-
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="w-full h-fit pb-0 lg:pb-20 lg:h-[90vh] bg-verdeagua">
-      <h1 className="text-center text-branco text-5xl font-calistoga mt-10">{t('relaxamentoTituloHome')}</h1>
-      <div className="w-[80%] h-fit flex lg:justify-around justify-center items-center lg:items-start mx-auto mt-32 lg:flex-row flex-col">
+      <h1 className="text-center text-branco xs:text-4xl md:text-5xl xl:text-7xl font-calistoga mt-10">
+        {t("relaxamentoTituloHome")}
+      </h1>
+      <div className="w-[80%] h-fit flex lg:justify-around justify-center items-center lg:items-start mx-auto xs:mt-16 lg:mt-28 lg:flex-row flex-col">
+        <Image
+          src={"/assets/usuario/relaxamento/meditacao.png"}
+          width={300}
+          height={300}
+          alt="..."
+          className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out"
+          onClick={() => funcao("m")}
+        />
 
-        <Image src={"/assets/usuario/relaxamento/meditacao.png"} width={300} height={300} alt="..." className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out" onClick={() => funcao("m")} />
+        <Image
+          src={"/assets/usuario/relaxamento/respiracao.png"}
+          width={300}
+          height={300}
+          alt="..."
+          className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out"
+          onClick={() => funcao("r")}
+        />
 
-        <Image src={"/assets/usuario/relaxamento/respiracao.png"} width={300} height={300} alt="..." className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out" onClick={() => funcao("r")}/>
-
-        <Image src={"/assets/usuario/relaxamento/yoga.png"} width={300} height={300} alt="..." className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out" onClick={() => funcao("y")}/>
+        <Image
+          src={"/assets/usuario/relaxamento/yoga.png"}
+          width={300}
+          height={300}
+          alt="..."
+          className="hover:cursor-pointer hover:scale-110 duration-500 ease-in-out"
+          onClick={() => funcao("y")}
+        />
       </div>
     </div>
-  )
+  );
 }
