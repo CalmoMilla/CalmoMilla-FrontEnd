4d3271f3ae7ecde457f8e14078cb63d7c5a3d507
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-29 13:07:49 -0300

    @selingindev  ~melhorando responsivo da modal das emoçoes

--- src/pages/api/urls.js
+++ src/pages/api/urls.js
@@ -1,2 +1,3 @@
-export const url = "http://localhost:8080/";
-// export const url = "http://ec2-18-230-88-220.sa-east-1.compute.amazonaws.com:8080/";
+export const urlLocal = "http://localhost:8080/";
+export const url =
+  "http://ec2-18-230-88-220.sa-east-1.compute.amazonaws.com:8080/";
