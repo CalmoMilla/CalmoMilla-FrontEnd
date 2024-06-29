730e5f60b3fb021168773763c07a78a758911df1
Arthur Selingin <arthur_santos@outlook.com.br>
2024-06-29 01:18:10 -0300

    @selingindev ~finalizando tradução pagina de login

--- src/app/[locale]/login/page.js
+++ src/app/[locale]/login/page.js
@@ -2,14 +2,18 @@ import initTranslations from "../../i18n";
 import TranslationsProvider from "../../TranslationProvider";
 import LoginGoogle from "@/pages/components/Login/LoginGoogle";
 
-const namespaces = ['home']
+const namespaces = ["login", "common"];
 
-export default async function Page({params: {locale} }) {
-  const { t, resources } = await initTranslations(locale, namespaces)
+export default async function Page({ params: { locale } }) {
+  const { t, resources } = await initTranslations(locale, namespaces);
 
   return (
-    <TranslationsProvider locale={locale} resources={resources} namespaces={namespaces}>
-      <LoginGoogle/>
+    <TranslationsProvider
+      locale={locale}
+      resources={resources}
+      namespaces={namespaces}
+    >
+      <LoginGoogle />
     </TranslationsProvider>
-  )
+  );
 }
