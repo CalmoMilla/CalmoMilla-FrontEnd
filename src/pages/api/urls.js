38cff1219031eb21866f439cd7eaa93085cc36d1
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2024-07-03 00:48:36 -0300

    Url errada

--- src/pages/api/urls.js
+++ src/pages/api/urls.js
@@ -1,3 +1,3 @@
-export const url = "http://localhost:8080/";
-// export const url =
-//   "http://ec2-18-230-88-220.sa-east-1.compute.amazonaws.com:8080/";
+//export const url = "http://localhost:8080/";
+export const url =
+  "http://ec2-18-230-88-220.sa-east-1.compute.amazonaws.com:8080/";
